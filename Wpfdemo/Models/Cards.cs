933802fde446add7287d88e2770479afc45c4096
Rizwanullah10 <113945217+Rizwanullah10@users.noreply.github.com>
2026-06-23 15:15:45 +0500

    made changes

---
+++ Wpfdemo/Models/Cards.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpfdemo.Models
+{
+    internal class cards
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
